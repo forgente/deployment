23dd878de439c3416cea07fa3a2366da8cd306e7
yp05327 <yp05327@noreply.gitea.com>
2025-01-16 15:50:21 +0000

    Auto allow eula (#5)
    https://github.com/travis-ci/travis-ci/issues/4714
    Reviewed-on: https://gitea.com/gitea/deployment/pulls/5
    Co-authored-by: yp05327 <yp05327@noreply.gitea.com>
    Co-committed-by: yp05327 <yp05327@noreply.gitea.com>

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -8,6 +8,7 @@ sudo apt update
 sudo apt install software-properties-common -y
 sudo apt-add-repository contrib -y
 sudo apt update
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt install ttf-mscorefonts-installer -y
 
 # playwright
