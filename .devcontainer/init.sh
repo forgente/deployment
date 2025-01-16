82644fa310be242c0da4374bccf6c738b1cc08b0
yp05327 <576951401@qq.com>
2025-01-16 07:43:38 +0000

    Add mscorefonts (#4)
    Reviewed-on: https://gitea.com/gitea/deployment/pulls/4
    Co-authored-by: yp05327 <576951401@qq.com>
    Co-committed-by: yp05327 <576951401@qq.com>

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -5,7 +5,10 @@ pip install -r requirements.txt
 
 # fonts
 sudo apt update
-sudo apt install fonts-noto -y
+sudo apt install software-properties-common -y
+sudo apt-add-repository contrib -y
+sudo apt update
+sudo apt install ttf-mscorefonts-installer -y
 
 # playwright
 playwright install --with-deps webkit
