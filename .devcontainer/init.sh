edabe894cdf32780a0359a74bec39984a7f874e7
yp05327 <576951401@qq.com>
2025-02-01 07:20:54 +0000

    Update .devcontainer/init.sh

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,14 +2,14 @@
 
 # fonts
 sudo apt update
-sudo apt install software-properties-common pipx -y
+sudo apt install software-properties-common -y
 sudo apt-add-repository contrib -y
 sudo apt update
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt install ttf-mscorefonts-installer -y
 
 # python libraries
-pipx install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 
 # playwright
 playwright install --with-deps webkit
