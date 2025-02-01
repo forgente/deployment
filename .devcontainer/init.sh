a6863de0075feb41a035d83bdc07e88cd0b58b1d
yp05327 <576951401@qq.com>
2025-02-01 07:12:15 +0000

    use pipx

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 
-# python libraries
-pip install -r requirements.txt
-
 # fonts
 sudo apt update
-sudo apt install software-properties-common -y
+sudo apt install software-properties-common pipx -y
 sudo apt-add-repository contrib -y
 sudo apt update
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt install ttf-mscorefonts-installer -y
+
+# python libraries
+pipx install -r requirements.txt
 
 # playwright
 playwright install --with-deps webkit
